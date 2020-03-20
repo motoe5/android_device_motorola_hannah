4a8ff2a2c3d0b09f64cc3a8da0bed5afb98d423c
wh0dat <whodat@remainsilent.net>
2020-03-20 07:13:19 +0530

    hannah: AEX BRiNGUP
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ $(call inherit-product, device/motorola/msm8937-common/msm8937.mk)
 $(call inherit-product-if-exists, vendor/motorola/hannah/hannah-vendor.mk)
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
